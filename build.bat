f71a174b2e2e4c56595ffff050ed2a77b7f58b28
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-06-20 08:37:28 -0400

    Remove some unused styles

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-cls
 for /R %%A in (*.md) do (
 	pandoc.exe -f markdown+startnum -t html5 --email-obfuscation=references --template="build/default.html5" -H "build/head.inc" -o "%%~dpnA.html" "%%A"
 )
