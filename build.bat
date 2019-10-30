13c10b1dcff026cd032f867bbcc6da26f97ff81c
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-10-30 15:05:28 -0400

    Add home link on each page

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 for /R %%A in (*.md) do (
-	pandoc.exe -f markdown+startnum-implicit_figures -t html5 --email-obfuscation=references --template="build/default.html5" -H "build/head.inc" -A "build/after.inc" -o "%%~dpnA.html" "%%A"
+	pandoc.exe -f markdown+startnum-implicit_figures -t html5 --email-obfuscation=references --template="build/default.html5" -H "build/head.inc" -A "build/after.inc" -B "build/before.inc" -o "%%~dpnA.html" "%%A"
 )
+	pandoc.exe -f markdown+startnum-implicit_figures -t html5 --email-obfuscation=references --template="build/default.html5" -H "build/head.inc" -A "build/after.inc" -o "index.html" "index.md"
