a2d5ca19e47558dfda549a75fe718b30b522d297
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-10-30 14:59:57 -0400

    Add AnchorJS

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 for /R %%A in (*.md) do (
-	pandoc.exe -f markdown+startnum-implicit_figures -t html5 --email-obfuscation=references --template="build/default.html5" -H "build/head.inc" -o "%%~dpnA.html" "%%A"
+	pandoc.exe -f markdown+startnum-implicit_figures -t html5 --email-obfuscation=references --template="build/default.html5" -H "build/head.inc" -A "build/after.inc" -o "%%~dpnA.html" "%%A"
 )
